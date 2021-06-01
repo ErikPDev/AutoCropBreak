eefe2d64d6d5570f459904adff66dcc21455c50b
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2021-06-01 21:46:42 +0300

    Added Copyright

--- src/ErikPDev/AutoCropBreak/Main.php
+++ src/ErikPDev/AutoCropBreak/Main.php
@@ -1,5 +1,35 @@
 <?php
 
+/*
+ *                _         _____                 ____                 _    
+ *     /\        | |       / ____|               |  _ \               | |   
+ *    /  \  _   _| |_ ___ | |     _ __ ___  _ __ | |_) |_ __ ___  __ _| | __
+ *   / /\ \| | | | __/ _ \| |    | '__/ _ \| '_ \|  _ <| '__/ _ \/ _` | |/ /
+ *  / ____ \ |_| | || (_) | |____| | | (_) | |_) | |_) | | |  __/ (_| |   < 
+ * /_/    \_\__,_|\__\___/ \_____|_|  \___/| .__/|____/|_|  \___|\__,_|_|\_\
+ *                                         | |                              
+ *                                         |_| By @ErikPDev for PMMP
+ *
+ * AutoCropBreak, a crop breaker plugin for PocketMine-MP
+ * Copyright (c) 2021 ErikPDev  < https://github.com/ErikPDev >
+ *
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * VoteParty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * ------------------------------------------------------------------------
+ */
+
 declare(strict_types=1);
 
 namespace ErikPDev\AutoCropBreak;
